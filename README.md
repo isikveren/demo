4f6662091607ad32c2bda7ebd88ffc362205952d
isikveren <49329148+isikveren@users.noreply.github.com>
2020-05-19 20:01:33 +0800

    5_19

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 2020年5月9日首次使用git与github连接。
 
-用于记录python学习库存 使用github存储代码
+用于记录python and c++学习库存 使用github存储代码
 
 书籍：Python编程：从入门到实践
 
---
+++ python/4_3.py
@@ -0,0 +1,9 @@
+#检查是否两个人都不小于二十一岁
+
+age_0 = 22
+age_1 = 18
+
+if (age_0 >= 21) and (age_1>=21):
+    print("true")
+else:
+    print("false")
